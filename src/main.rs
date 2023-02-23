ed2513c6523d1d6788222c994b40b549cc3decdc
Austin Jones <jonesaustindev@gmail.com>
2023-02-22 19:45:53 -0600

    working on adding sprites

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,70 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PresentMode};
+
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+pub const RESOLUTION: f32 = 16.0 / 9.0;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    let height = 800.0;
+
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        // .insert_resource(ImageSettings::default_nearest())
+        .add_startup_system_to_stage(StartupStage::PreStartup, load_ascii)
+        .add_startup_system(spawn_camera)
+        .add_startup_system(spawn_character)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                title: "Poke Test".to_string(),
+                width: height * RESOLUTION,
+                height: height,
+                present_mode: PresentMode::Fifo,
+                resizable: false,
+                ..default()
+            },
+            ..default()
+        }))
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn spawn_character(mut commands: Commands, ascii: Res<AsciiSheet>) {
+    let mut sprite = TextureAtlasSprite::new(1);
+    sprite.color = Color::rgb(0.3, 0.3, 0.9);
+    sprite.custom_size = Some(Vec2::splat(1.0));
+
+    commands
+        .spawn(SpriteSheetBundle {
+            sprite,
+            texture_atlas: ascii.0.clone(),
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 900.0),
+                ..Default::default()
+            },
+            ..default()
+        })
+        .insert(Name::new("Player"));
+}
+
+struct AsciiSheet(Handle<TextureAtlas>);
+
+fn load_ascii(
+    mut commands: Commands,
+    assets: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let image = assets.load("Ascii.png");
+    let atlas = TextureAtlas::from_grid(
+        image,
+        Vec2::splat(9.0),
+        16,
+        16,
+        Some(Vec2::splat(2.0)),
+        None,
+    );
+
+    let handle = texture_atlases.add(atlas);
+    commands.insert_resource(AsciiSheet(handle));
 }
